ecd0f198e475c7e1945bd12be547708a6a9b0165
Venky <venkaiah.p@zedbee.in>
2023-06-14 18:41:17 +0530

    First COmmit

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import React from 'react';
+const Navbar = () => {
+    return (
+        <div className='max-w-[100vw] h-[72px] top-[0px] sticky z-[150] bg-[#000] rounded-lg shadow-[rgb(229, 232, 235) 0px ]'>
+            <div className='my-[0px] mx-auto max-w-[2560px] w-[100%] height-[100%] py-0 px-[32px]'>
+                <nav className='flex h-[100%] justify-between'>
+                    //
+                    <div></div>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
